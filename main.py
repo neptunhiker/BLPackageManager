25c718b2dce1997747e000d57f076f020bede548
neptunhiker <neptunhiker@47.com>
2023-04-07 21:35:13 +0200

    Create app class, create settings, and directory from frames

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello world")
+import ttkbootstrap as ttk
+
+
+import settings
+
+
+class App(ttk.Window):
+
+    def __init__(self):
+        super(App, self).__init__(themename=settings.THEMENAME)
+        self._full_screen_window()
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+    
+    def _full_screen_window(self) -> None:
+        """
+        Sets the screen to full size
+        :return None
+        """
+        w, h = self.winfo_screenwidth(), self.winfo_screenheight()
+        self.geometry("%dx%d+0+0" % (w, h))  # sets screen to full size
+        self.state('zoomed')
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
