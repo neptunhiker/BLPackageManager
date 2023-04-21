ee1337e8960afb11d695395567edd4e5892ef4c5
neptunhiker <neptunhiker@47.com>
2023-04-21 22:14:40 +0200

    Add ttk themes

---
+++ themes/bl_dark.py
@@ -0,0 +1,107 @@
+USER_THEMES = {
+    "beginnerluft": {
+        "type": "light",
+        "colors": {
+            "primary": "#ffff00",
+            "secondary": "#dcdcdc",
+            "success": "#9437ff",
+            "info": "#bd9398",
+            "warning": "#f0ad4e",
+            "danger": "#d9534f",
+            "light": "#d5d5d5",
+            "dark": "#000000",
+            "bg": "#eaeaea",
+            "fg": "#212121",
+            "selectbg": "#fefb00",
+            "selectfg": "#212121",
+            "border": "#bfbfbf",
+            "inputfg": "#343a40",
+            "inputbg": "#fff",
+            "active": "#e5e5e5"
+        }
+    },
+    "beginnerluftclassic": {
+        "type": "light",
+        "colors": {
+            "primary": "#ffff00",
+            "secondary": "#dcdcdc",
+            "success": "#00f900",
+            "info": "#fefc78",
+            "warning": "#f0ad4e",
+            "danger": "#d9534f",
+            "light": "#d5d5d5",
+            "dark": "#000000",
+            "bg": "#feffff",
+            "fg": "#212121",
+            "selectbg": "#fefb00",
+            "selectfg": "#212121",
+            "border": "#bfbfbf",
+            "inputfg": "#343a40",
+            "inputbg": "#fff",
+            "active": "#e5e5e5"
+        }
+    },
+    "beginnerluftdark": {
+        "type": "light",
+        "colors": {
+            "primary": "#ffff00",
+            "secondary": "#dcdcdc",
+            "success": "#00f900",
+            "info": "#fefc78",
+            "warning": "#f0ad4e",
+            "danger": "#d9534f",
+            "light": "#d5d5d5",
+            "dark": "#000000",
+            "bg": "#000000",
+            "fg": "#eaeaea",
+            "selectbg": "#fefb00",
+            "selectfg": "#212121",
+            "border": "#bfbfbf",
+            "inputfg": "#343a40",
+            "inputbg": "#fff",
+            "active": "#e5e5e5"
+        }
+    },
+    "beginnerluftearthy": {
+        "type": "light",
+        "colors": {
+            "primary": "#183618",
+            "secondary": "#dda15e",
+            "success": "#bc6c25",
+            "info": "#dda15e",
+            "warning": "#bc6c25",
+            "danger": "#d9534f",
+            "light": "#d5d5d5",
+            "dark": "#000000",
+            "bg": "#606c38",
+            "fg": "#eaeaea",
+            "selectbg": "#fefb00",
+            "selectfg": "#212121",
+            "border": "#bfbfbf",
+            "inputfg": "#343a40",
+            "inputbg": "#fff",
+            "active": "#e5e5e5"
+        }
+    },
+    "newtheme": {
+        "type": "light",
+        "colors": {
+            "primary": "#ffff00",
+            "secondary": "#dcdcdc",
+            "success": "#00f900",
+            "info": "#fefc78",
+            "warning": "#f0ad4e",
+            "danger": "#d9534f",
+            "light": "#d5d5d5",
+            "dark": "#000000",
+            "bg": "#000000",
+            "fg": "#eaeaea",
+            "selectbg": "#fefb00",
+            "selectfg": "#212121",
+            "border": "#bfbfbf",
+            "inputfg": "#343a40",
+            "inputbg": "#fff",
+            "active": "#e5e5e5"
+        }
+    }
+}
